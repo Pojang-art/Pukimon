88a8b330b1afc11a9afbbb1bd1c18af4cf170a72
Jang <feinsgaming@gmail.com>
2026-05-04 13:24:09 +0700

    Update Supabase URL and key in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED oleh build.cjs (jangan diedit manual; nilai diambil dari env Cloudflare Pages).
-window.SB_URL = "YOUR_SUPABASE_URL";
-window.SB_KEY = "YOUR_SUPABASE_ANON_KEY";
+window.SB_URL = "https://tqrdnomgalnuiegxrdbp.supabase.co";
+window.SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxcmRub21nYWxudWllZ3hyZGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMDgxNjMsImV4cCI6MjA5MDY4NDE2M30.jGxoVyozvWGDPQpws7JSkfwBsr5w-2vi-sPEwmP1xwQ";
